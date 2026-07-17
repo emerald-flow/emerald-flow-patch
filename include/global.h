b4b29dc7bc85e952f74c5e129fb2e2ef7b40cf9b
officer-kd6-3dot7 <punithg360@gmail.com>
2026-07-17 19:49:14 +0530

    Issue #1 - Missing Pokedex entries after applying patch

--- include/global.h
+++ include/global.h
@@ -1095,9 +1095,8 @@ struct SaveBlock1
     /*0x31DC*/ struct Roamer roamer;
     /*0x31F8*/ struct EnigmaBerry enigmaBerry;
     /*0x322C*/ struct MysteryGiftSave mysteryGift;
-    /*0x3598*/ u8 unused_3598;
-               u16 adoptionSpecies[1];
-               u8 unused_359B[381];
+    /*0x3598*/ u16 adoptionSpecies[1]; // Oh god. u16 and even addresses
+               u8 unused_359B[0x17E];
     /*0x3718*/ u32 trainerHillTimes[NUM_TRAINER_HILL_MODES];
     /*0x3728*/ struct RamScript ramScript;
     /*0x3B14*/ struct RecordMixingGift recordMixingGift;
